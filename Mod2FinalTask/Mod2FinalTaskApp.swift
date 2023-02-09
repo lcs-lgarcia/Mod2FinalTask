1dd40caa08eb62d7ea1523e1de7e21741f82ab30
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-02-09 09:21:07 -0500

    Initial Commit

---
+++ Mod2FinalTask/Mod2FinalTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mod2FinalTaskApp.swift
+//  Mod2FinalTask
+//
+//  Created by Lucas García on 9/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Mod2FinalTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
