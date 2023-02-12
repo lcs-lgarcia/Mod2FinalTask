3e717c30de6978b1d9fe693fa9f82e9d9faa69e6
Lucas Garcia <lgarciaalonso@lcs.on.ca>
2023-02-11 19:30:37 -0500

    A lot of progress in this version, added some optionals and variables, and fixed the error of the list view. I included the price over the m3, also put the decimal format on some numbers and I have done the calculations over the m3 creating a first price of the product. Also added a new picker that gives you the option of selecting the type of ground appart from the percentage of this one.

--- Mod2FinalTask/CreateTank.swift
+++ Mod2FinalTask/CreateTank.swift
@@ -9,62 +9,98 @@ import SwiftUI
 
 struct CreateTank: View {
     
-    @State var defaultGroundPercentage = 25
+    @State var defaultGroundPercentage = 10
     let groundPercentages = [10, 15, 20, 0]
+    @State var defaultGroundType = "SAND"
+    let typeground = ["SAND", "ROCK", "MIX"]
     @State var priorResults: [Result] = []
-    @State var lenght: Double = 0.1
-    @State var height: Double = 0.1
-    @State var width: Double = 0.1
+    @State var lenght: Double = 0.2
+    @State var height: Double = 0.2
+    @State var width: Double = 0.2
     @State var nameOfTheOrder: String = "Pez"
- 
-    var tipPercentage: Double {
+    var m3: Double {
         return lenght*height*width
     }
-  
+    var price: Double {
+        return m3*7.23
+    }
+   
     
     
     var body: some View {
         VStack{
             
+            Divider()
             VStack(spacing:0){
                 
                 Text("Height")
                     .font(.title2)
                     .bold()
                 Slider(value: $height,
-                       in: 0.1...10,
+                       in: 0.2...10,
                        label: {Text("radius")},
-                       minimumValueLabel: { Text("0.1") },
+                       minimumValueLabel: { Text("0.2") },
                        maximumValueLabel: { Text("10") })
                 
-                Text("\(Decimal(height).formatted(.number.precision(.significantDigits(3))))")
+                Text("\(Decimal(height).formatted(.number.precision(.significantDigits(2)))) m")
                     .padding()
                 Text("Width")
                     .font(.title2)
                     .bold()
                 Slider(value: $width,
-                       in: 0.1...10,
+                       in: 0.2...10,
                        label: {Text("width")},
-                       minimumValueLabel: { Text("0.1") },
+                       minimumValueLabel: { Text("0.2") },
                        maximumValueLabel: { Text("10") })
                 
-                Text("\(Decimal(width).formatted(.number.precision(.significantDigits(3))))")
+                Text("\(Decimal(width).formatted(.number.precision(.significantDigits(2)))) m")
                     .padding()
                 Text("Lenght")
                     .font(.title2)
                     .bold()
                 Slider(value: $lenght,
-                       in: 0.1...10,
+                       in: 0.2...10,
                        label: {Text("lenght")},
-                       minimumValueLabel: { Text("0.1") },
+                       minimumValueLabel: { Text("0.2") },
                        maximumValueLabel: { Text("10") })
-                
-                Text("\(Decimal(lenght).formatted(.number.precision(.significantDigits(3))))")
+                Text("$/m³ = 7.32 ")
+                Text("\(Decimal(lenght).formatted(.number.precision(.significantDigits(2)))) m")
                     .padding()
                 
                 
             }
-            Text("m3 = \(tipPercentage)")
+            
+            Text("m³ = \(Decimal(m3).formatted(.number.precision(.significantDigits(2))))")
+            Divider()
+            Group {
+                
+                HStack {
+                    Text("Type of ground")
+                        .font(.headline.smallCaps())
+                    
+                    Spacer()
+                }
+                .padding(.horizontal)
+                
+                Picker("Type of ground",
+                       selection: $defaultGroundType) {
+                    ForEach(typeground, id: \.self) { currentPercentage in
+                        Text("\(currentPercentage)")
+                            .tag(currentPercentage)
+                    }
+                }
+                       .pickerStyle(.segmented)
+                       .padding()
+                HStack{
+                    Text("    2,22$/m³")
+                    Spacer()
+                    Text("4,07$/m³")
+                    Spacer()
+                    Text("3,46$/m³    ")
+                }
+                .padding()
+            }
+            Divider()
             Group {
                 
                 HStack {
@@ -86,6 +122,9 @@ struct CreateTank: View {
                        .padding()
                 
             }
+            
+            Text("Total price of the tank:\(Decimal(price).formatted(.number.precision(.significantDigits(3)))) $")
+            
             Button(action: {
                 let latestResult = Result(weight: 2, height: 2, lenght: 2, order: "seso", nameOfTheOrder: "caca")
                 priorResults.append(latestResult)
--- Mod2FinalTask/Orders.swift
+++ Mod2FinalTask/Orders.swift
@@ -12,20 +12,20 @@ struct Orders: View {
     @State var priorResults: [Result] = []
     
     var body: some View {
-        
-        HStack{
-            Spacer()
-            Text ("History")
-                .font(.title)
-                .fontWeight(.bold)
-            Spacer()
-        }
-        List(priorResults.reversed()) { currentResult in
-           
+        VStack{
+            HStack{
+                Spacer()
+                Text ("YOUR ORDERS")
+                    .font(.title)
+                    .fontWeight(.bold)
+                Spacer()
+            }
+            List(priorResults.reversed()) { currentResult in
+                
             }
         }
     }
-
+}
 
 struct Orders_Previews: PreviewProvider {
     static var previews: some View {
